678bd8d2d6633d7ebafca785f8bd46f0f07ee8d9
dark0o <darko_crnic@hotmail.com>
2021-07-23 11:08:56 +0200

    Add first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AlbumsComponent,
+    PhotosComponent
   ],
   imports: [
     BrowserModule
